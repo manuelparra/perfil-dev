bcc0f14202245af5cec88995a68aa03ab94362c5
Manuel Parra <manuelparra@live.com.ar>
2019-09-08 17:29:31 -0400

    Incluimos un archivo index.js para las el efecto de scroll

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+function loadfunctions() {
+    // after the page load, we charged all this functions
+
+    function goToByScroll(id){
+        // Reove "link" from the ID
+
+        id = id.replace("_scroll", "");
+
+        // Scroll
+        $('html, body').animate({
+            scrollTop: $("#"+id).offset().top
+        }, 1000);
+    }
+
+    $("#navbarText > ul > li > a").click(function(e) {
+            // Prevent a page reload when a link is pressed
+            if ($(this).attr('href').slice(0, 1) == "#") {
+                e.preventDefault();
+
+                // Call the scroll function
+                goToByScroll($(this).attr("id"));
+            }
+    });
+
+    $("#banner > a.goto-next").click(function(e) {
+            // Prevent a page reload when a link is pressed
+            e.preventDefault();
+            // Call the scroll function
+            goToByScroll($(this).attr("id"));
+    });
+
+    $("#banner > a.btn-contactame").click(function(e) {
+            // Prevent a page reload when a link is pressed
+            e.preventDefault();
+            // Call the scroll function
+            goToByScroll($(this).attr("id"));
+    });
+}
+
+window.addEventListener('load', loadfunctions, false);
